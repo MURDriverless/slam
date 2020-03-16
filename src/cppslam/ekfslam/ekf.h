b1886e4f196e3f5b58c5dcdae9bb830c9d6230bc
Jack McRobbie <jackmcrobbie@hotmail.com>
2020-03-16 22:01:19 +1100

    More development, began implementing basic motion model

--- src/cppslam/ekfslam/ekf.h
+++ src/cppslam/ekfslam/ekf.h
@@ -4,18 +4,20 @@
 #include "../matrix/matrixUtils.h"
 #include "ros/ros.h"
 #include <sensor_msgs/PointCloud2.h>
-
+#include <math.h>
 
 class ekf {
 
 public:
 	ekf(ros::NodeHandle n);
-
-
 	void runnable(int hz,Matrix initialState);
+
 private:
+	int initialiseSubs();
+	Matrix motionModel(Matrix &x, Matrix &u);
 	int stateSize;
 	int controlSize;
 	ros::NodeHandle nh;
+	double dt; 	
 };
 #endif
--- src/cppslam/slamNode.cpp
+++ src/cppslam/slamNode.cpp
@@ -3,26 +3,18 @@
 #include <sstream>
 #include "ekfslam/ekf.h"
 
-int main (int argc, char **argv)
-{
-	ros::init(argc, argv, "slamNode");
-	ros::NodeHandle n; 
-	ROS_INFO_STREAM("Slam node started");
-	
-}
-
-
-
 int main(int argc, char **argv)
 {
 	ros::init(argc,argv,"slamNode");
 
-	ros::nodeHandle n; 
+	ros::NodeHandle n; 
 
 	ROS_INFO_STREAM("MUR SLAM: LAUNCHED");
 
 
-	
-	return;
+	ekf slam(n); 
 
+	Matrix X0(4,1);
+	X0.Fill(0.0);
+	slam.runnable(10, X0);	
 }
