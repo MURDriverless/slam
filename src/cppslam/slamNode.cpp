dc424bfeef9b84a2ded9048eece26fc05fb6ed57
Jack McRobbie <jackmcrobbie@hotmail.com>
2020-04-20 17:17:24 +1000

    added comment to main node manager

--- src/cppslam/slamNode.cpp
+++ src/cppslam/slamNode.cpp
@@ -5,6 +5,10 @@
 
 int main(int argc, char **argv)
 {
+/* High level manager for the slam node. 
+Will call a class of slam that has been developed in subfolders to this directory. 
+Currently only has ekf slam.	
+ */
 	ros::init(argc, argv, "slamNode");
 
 	ros::NodeHandle n;
