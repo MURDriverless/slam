16963523550d9d1854c23794efd9e72cd8db2797
Jack McRobbie <jackmcrobbie@hotmail.com>
2020-04-28 02:47:28 +1000

    Reimplemented point class without eigen templating

--- src/cppslam/point/point.h
+++ src/cppslam/point/point.h
@@ -1,4 +1,5 @@
 #include <Eigen/Dense>
+#include <string>
 /*
     This class implements points using templated Eigen Matrix functions 
     To represent points in an XYZ format.
@@ -6,25 +7,28 @@
 */
 
   template<typename T>
-  class Point : public Eigen::Matrix<T,3,1>
+  class Point
   {
-    typedef Eigen::Matrix<T,3,1> BaseClass;
-  
   public:
-    Point () 
-      : BaseClass()
+    Point()
     {}
-    // 3-element constructor, delegates to base class constructor
-    Point(const T& x, const T& y, const T& z)
-      : BaseClass(x, y, z)
-    {}
-  
-    // Copy constructor from any Eigen matrix type
-    template<typename OtherDerived>
-    Point(const Eigen::MatrixBase<OtherDerived>& other)
-      : BaseClass(other)
-    {}
-  
-    // Reuse assignment operators from base class
-    using BaseClass::operator=;
+
+    Point(T x_in, T y_in, T z_in, std::string colour_in)
+      {
+        x = x_in;
+        y = y_in;
+        z = z_in;
+        colour = colour_in; 
+      }
+    Point(T x_in, T y_in, T z_in)
+      {
+        x = x_in;
+        y = y_in;
+        z = z_in;
+      }
+    T x; 
+    T y; 
+    T z;
+    std::string colour;
+
   };
